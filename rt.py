28d3e1454daee19cbd2c1ac4e981b104017ca468
Paul Hoffman <phoffman@proper.com>
2018-12-03 13:06:01 -0800

    More general progress

--- rt.py
+++ rt.py
@@ -5,22 +5,37 @@ See https://github.com/icann/resolver-testbed for more information
 Must be run in the same directory as the config files
 '''
 
-import glob, json, os, shutil, subprocess, sys, time, logging
+import os, subprocess, sys, time, logging, configparser
 import fabric
 
-# Constants
-PROG_DIR = os.getcwd()
-LOG_FILE = "{}/Local/log_resolver_testbed.txt".format(PROG_DIR)
-RESOLVER_TEST_LOWEST_BASE = "restest1604"
-PROVISION_FILE = "run_at_start.sh"
-TCPDUMP_PREFIX = "tcpdump -i"
-SUDO_NOHUP_TCPDUMP_PREFIX = "sudo nohup tcpdump -i"
-VM_CONFIG = "{}/config_for_vms.json".format(PROG_DIR)
-BUILD_CONFIG = "{}/config_for_rt_builds.json".format(PROG_DIR)
+# Some program-wide constants
+PROG_DIR = os.path.abspath(os.getcwd())
+LOG_FILE = "{}/log_resolver_testbed.txt".format(PROG_DIR)
+LOCAL_CONFIG = "{}/local_config.txt".format(PROG_DIR)
+BUILD_CONFIG = "{}/build_config.json".format(PROG_DIR)
+
+VM_INFO = {
+    "gateway-vm": {
+        "control_addr": "192.168.56.2"
+    },
+    "servers-vm": {
+        "control_addr": "192.168.56.3"
+    },
+    "resovers-vm": {
+        "control_addr": "192.168.56.4"
+    }
+}
+
+CLI_COMMANDS = [
+"help",
+"setup_vm"
+]
 
 HELP_TEXT = '''
-Available commands are:
-help:           Show this text
+Available commands for rt.py are:
+help                 Show this text
+setup_vm <vmname>    Sets up a VM for the first time;
+                     must be run as roon on the VM itself
 '''.strip()
 
 # Do very early check for contents of the directory that we're running in
@@ -48,72 +63,128 @@ def die(in_str):
     exit()
 
 def show_help():
-    ''' show some help text to the user '''
     print(HELP_TEXT)
 
-def cmd_to_vm(cmd_to_run, vm_target):
-    ''' Runs a command on a named VM. Returns list of [ success_boolean, output_text ] '''
-    if not vm_target in vm_info:
-        die("Attempt to run on {}, which is not a valid VM".format(vm_target))
-    if vm_target == "resolvers-vm":
-        is_resolvers_vm_running()
-    ######## More goes here
-
-def is_resolvers_vm_running():
-    ''' Check if the resolvers-vm is running; die if not '''
-    ############ More goes here
-
-def initialize_control_host():
-    ''' Make sure everything on the control host is set up; might die if it can't '''
-    ############### More goes here
-
-def config_from_json(in_file):
-    ''' Return the object in a JSON file '''
-    if not os.path.exists(in_file):
-        die("Could not find {}.".format(in_file))
-    f = open(in_file, mode="rt")
+def cmd_to_vm(cmds_to_run, vm_name):
+    ''' Runs a list of commands on a named VM. Returns list of [ success_boolean, output_text ] '''
+    if not vm_name in VM_INFO:
+        die("Attempt to run on {}, which is not a valid VM".format(vm_name))
+    is_vm_running(vm_name)
+    this_control_address = (rt_config["vm_info"][vm_name]).get("control_addr")
+    if not this_control_address:
+        die("There was no address for {}".format(vm_name))
+    fabconn = fabric.Connection(host=this_control_address, user="root")
     try:
-        this_config = json.load(f)
-        return this_config
+        fabconn.open()
     except:
-        die("Could not parse the JSON in {}.".format(in_file))
-    ################ More goes here
+        die("Could not open an SSH connection to {}.".format(vm_name))
+    # Is this the right VM?
+    try:
+        ret_text = fabconn.run("hostname")
+    except Exception as this_e:
+        die("Could not run hostname on {}: '{}'".format(vm_name, this_e))
+    if ret_text.strip() != vm_name:
+        die("The host at {} is not {}: '{}'".format(this_control_address, vm_name, ret_text.strip()))
+    # Find the commands
+    #   The argument can be a list or a string
+    if isinstance(cmds_to_run, list):
+        commands_list = cmds_to_run
+    elif isinstance(cmds_to_run, str):
+        commands_list = [ cmds_to_run ]
+    else:
+        die("The command going to cmd_to_vm has to be a list or a string.")
+    # Run the commands
+    for this_command in commands_list:
+        try:
+            ret_text = fabconn.run(this_command)
+        except Exception as this_e:
+            die("Could not run '{}' on {}: '{}'".format(this_command, vm_name, this_e))
 
-def run_main():
-    ''' The main program; always exits '''
+def is_vm_running(vm_name):
+    ''' Check if the VM is running; die if not '''
+    p = subprocess.Popen("VBoxManage runningvms", stdout=subprocess.PIPE, shell=True)  
+    ret_val = p.wait()
+    if ret_val > 0:
+        die("VBoxManage runningvms failed to run.")
+    the_running_vms = (p.stdout.read()).decode("latin-1").strip().split()
+    if not vm_name in the_running_vms:
+        die("{} is not in the list of running VMs: '{}'.".format(vm_name, " ".join(the_running_vms)))
+
+def startup_and_config():
+    ''' Make sure everything on the control host is set up correctly, and die if it is not; returns local configuration '''
+    # Be sure we are running from the directory the program is in
+    if sys.argv[0] != "./rt.py":
+        die("This program must be run as ./rt.py so that all the additional files are found.")
+    # Make sure certain files exist
+    for this_file in [LOCAL_CONFIG, BUILD_CONFIG]:
+        if not os.path.exists(this_file):
+            die("{} was not found, but that file is needed.".format(this_file))
+    # Parse the configuration
+    c = configparser.ConfigParser()
+    try:
+        c.read(LOCAL_CONFIG)
+    except:
+        die("The configuration in {} was malformed.".format(LOCAL_CONFIG))
+    if not "resolver" in c.sections:
+        die("The configuration in {} does not have a [resolver]section.".format(LOCAL_CONFIG))
+    this_local_config = {}
+    for this_key in c["resolver"]:
+        this_local_config[this_key] = c["resolver"][this_key]
+    # Be sure the required configuration bits are there
+    if not this_local_config.get("local_internet_interface"):
+        die("The configuration in {} did not include a local_internet_interface item.".format(LOCAL_CONFIG))
+    if this_local_config["local_internet_interface"]:
+        die("The configuration in {} had a blank local_internet_interface item.".format(LOCAL_CONFIG))
+    # Make sure that vboxmanage is available
+    try:
+        subprocess.check_call("VBoxManage --version", shell=True)
+    except:
+        die("Could not run VBoxManage during sanity check.")
+    # Make sure the three VMs at least exist
+    for this_vm_name in VM_INFO:
+        try:
+            subprocess.check_call("VBoxManage showvminfo {}".format(this_vm_name))
+        except:
+            die("Could not find {} in the VirtualBox inventory.".format(this_vm_name))
+    # Add VM_INFO to the local configuration
+    this_local_config["vm_info"] = {}
+    for this_key in VM_INFO:
+        this_local_config["vm_info"][this_key] = VM_INFO[this_key]
+    # Finish up initialization
+    return this_local_config
+
+def do_setup_vm(this_vm):
+    ''' Do initial setup on a VM; dies if the VM is not ready '''
+    if this_vm == "" or (" " in this_vm):
+        die("The setup_vm command takes one argument: the name of the VM")
+    vm_list = (rt_config["vm_info"]).keys()
+    if not this_vm in vm_list:
+        die("The argument to setup_vm must be a VM name from '{}'".format(" ".join(vm_list)))
+    ######### More here
+    # Change the hostname to this_vm
+    # Add the /etc/hosts/interfaces file
+    # Do VM-specific setup
+    
+
+# Run the main program
+if __name__ == "__main__":
     log("## Starting run on date {}".format(time.strftime("%Y-%m-%d")))
     # Parse the input
     if len(sys.argv) < 2:
         show_help()
         die("There were no arguments on the command line.")
-    # Make sure that vboxmanage is available
-    try:
-        subprocess.check_call("VBoxManage --version", shell=True)
-    except:
-        die("Could not run VBoxManage at start of program.")
-    # Get the configuration for VMs
-    vm_info = config_from_json(VM_CONFIG)
-    initialize_control_host()  # Make sure everything is set up correctly
-    # Make sure the needed VMs are running
-    for this_vm_name in vm_info:
-        try:
-            subprocess.check_call("VBoxManage showvminfo {}".format(this_vm_name))
-        except:
-            die("Could not find {} in the VirtualBox inventory.".format(this_vm_name))
-    ##### More goes here
+    rt_config = startup_and_config()  # Get the config, and make sure everything is set up correctly
     # Get the command
     cmd = sys.argv[1]
     cmd_args = sys.argv[2:]
     log("Command was {} {}".format(cmd, " ".join(cmd_args)))
+    if not cmd in CLI_COMMANDS:
+        show_help()
+        die("{} is not valid command.".format(cmd))
     # Figure out which command it was
-    if cmd == "help":  # Help
+    if cmd == "help":
         show_help()
-    else:
-        log("'{}' is not a valid command.".format(cmd))
-        show_help()
+    elif cmd == "setup_vm":
+        do_setup_vm(cmd_args)
     # We're done, so exit
     exit()
-
-# Run the main program
-if __name__ == "__main__":
-    run_main()
--- setup-and-running.md
+++ setup-and-running.md
@@ -13,50 +13,100 @@ The last two steps can be repeated as the configurations change for different te
 
 ## Building the Base VM Image
 
-* Get the recent Debiain image from `http://cdimage.debian.org/cdimage/release/current/amd64/iso-cd/debian-9.5.0-amd64-netinst.iso`
+* Get the recent Debiain image from `http://cdimage.debian.org/cdimage/release/current/amd64/iso-cd/debian-9.6.0-amd64-netinst.iso`
 
 * Choose which SSH keys you will use for logging into the VMs on the testbed.
 This needs to have the private key _not_ password-protected, so you might want to create a new keypair for the testbed.
 To ease installation, you might put this as an authized_keys file on a locally-managed web server.
 
 * Start Virtualbox
-	* Machine&rarr;New
-		* Name: debian950-base
+	* Machine &rarr; New
+		* Name: debian960-base
+		* Type: Linux
+		* Version: Debian (64-bit)
 		* Memory: 2048M
-		* Drive: VDI, dynamically-allocated, 20 gig
+		* Create new virtual hard drive
+			* Type: VDI
+			* Storage: dynamically allocated
+			* Size: 20 gig
 
 * Before booting, change settings
-	* System&rarr;Motherboard: Uncheck floppy
-	* System&rarr;Motherboard: Pointing Device: PS2 Mouse, 
-	* System&rarr;Processor: 2 CPUs
-	* Storage&rarr;Controller: IDE: Channge "empty" to attach the Debian ISO from above
-	* Network&rarr;Adatper 1: Attached to "Bridged Adapter" on the NIC for your computer that leads to the Internet
-	* Ports&rarr;USB: off
+	* System &rarr; Motherboard: Uncheck floppy
+	* System &rarr; Motherboard: Pointing Device: PS2 Mouse, 
+	* System &rarr; Processor: 2 CPUs
+	* Storage &rarr; Controller: IDE: Channge "empty" to attach the Debian ISO from above
+	* Network &rarr; Adatper 1: Attached to "Bridged Adapter" on the NIC for your computer that leads to the Internet
+	* Ports &rarr; USB: off
 
-* Boot the new `debian950-base` VM
-	* Use Non-graphical installation, and use the default choices other than what is listed here
-		* Hostname: debian950-base
-		* Domain name: don't enter one
-		* Root password: this will be used for all interaction with all VMs, so use a strong one
+* Boot the new `debian960-base` VM
+	* Use Non-graphical installation, and use the default choices other than these:
+		* Hostname: debian960-base
+		* Domain name: Make sure this is blank
+		* Root password: This will be used for all interaction with all VMs, so use a strong one
 		* User: Any name, any password (this user will not be used in the testbed)
-		* Disk partitioning: Guided, use entire disk, all files in one partition
+		* Time zone: Use your local time zone
+		* Partition disks:
+			* Guided - use entire disk
+			* All files in one partition
+		* Configure the package manager
+			* No additional CDs
+			* Any mirror is fine; Use the default
+			* Popularity contest: No
 		* Software selection
 			* Unselect "Debian desktop environment"
 			* Unselect "print server"
-			* Select SSH server
+			* Select "SSH server"
+			* Leave "standard system utilities" selected
 
 * After automatic reboot
 	* Log in as root with the password created above
-	* `apt update`
-	* `apt -y upgrade`
-	* `mkdir .ssh`
-	* `chmod 700 .ssh`
-	* `cd .ssh`
-	* Install the authorized_keys file, possibly by getting it off of the locally-administered web server
-	* `chmod 600 authorized_keys`
-	* `sudo shutdown -h now`
+	* General machine preparation
+		* `apt update`
+		* `apt -y upgrade`
+		* `apt -y install build-essential git`
+	* Get the project repo so that the VMs can be set up easily
+		* `git clone https://github.com/icann/resolver-testbed.git`
+	* Set up SSH for automated logging in
+		* `mkdir .ssh`
+		* `chmod 700 .ssh`
+		* `cd .ssh`
+		* Install the authorized_keys file, possibly by getting it off of the locally-administered web server
+		* `chmod 600 authorized_keys`
+	* `shutdown -h now`
 
-## Clone the Base VM Image to the other VMs
+## Clone the Base VM Image to the Other VMs
 
-@@ MORE STUFF GOES HERE
+All clones are full clones because they are faster.
+
+### Gateway VM
+
+* Select debian96-base in the VirtualBox UI
+* Machine &rarr; Clone
+	* Name: gateway-vm
+	* Clone type: Full clone
+* Machine &rarr; Settings
+	* Network &rarr; Adatper 1: Attached to "Host-only Adapter" _vboxnet0_
+	* Network &rarr; Adatper 2: Attached to "Internal Network" _resnet_
+	* Network &rarr; Adatper 3: Attached to "Internal Network" _servnet_
+	* Network &rarr; Adatper 4: Attached to "Bridged Adapter" on the NIC for your computer that leads to the Internet
+
+### Root Servers VM
+
+* Select debian96-base in the VirtualBox UI
+* Machine &rarr; Clone
+	* Name: servers-vm
+	* Clone type: Full clone
+* Machine &rarr; Settings
+	* Network &rarr; Adatper 1: Attached to "Host-only Adapter" _vboxnet0_
+	* Network &rarr; Adatper 2: Attached to "Internal Network" _servnet_
+
+### Resolver Systems
+
+* Select debian96-base in the VirtualBox UI
+* Machine &rarr; Clone
+	* Name: resovers-vm
+	* Clone type: Full clone
+* Machine &rarr; Settings
+	* Network &rarr; Adatper 1: Attached to "Host-only Adapter" _vboxnet0_
+	* Network &rarr; Adatper 2: Attached to "Internal Network" _resnet_
 

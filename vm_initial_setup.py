51bac51940e3b6fb27cef595cf0ee6c5fc8cfd01
Paul Hoffman <phoffman@proper.com>
2018-12-03 16:38:37 -0800

    Mostly done on local VM setup

--- vm_initial_setup.py
+++ vm_initial_setup.py
@@ -3,27 +3,51 @@
 Run on freshly-made resolvers to install things that are needed
 '''
 
-import os, subprocess, sys
+import os, subprocess, sys, shutil
 
 # Some program-wide constants
 PROG_DIR = os.path.abspath(os.getcwd())
 CLONE_BASENAME = "debian960-base"
+OK_VM_LIST = [ "gateway-vm", "servers-vm", "control_addr" ]
+
+def die(in_str):
+    ''' log then exit  '''
+    err_str = in_str + " Exiting."
+    print(err_str)
+    exit()
 
 # Run the main program
 if __name__ == "__main__":
     ''' Do initial setup on a VM; dies if the VM is not ready '''
+    if len(sys.argv) == 1:
+        die("Must give one argument: the name of this vm. Must be one of '{}'.".format(" ".join(OK_VM_LIST)))
     in_vm_name = sys.argv[1]
-    ok_vm_list = [ "gateway-vm", "servers-vm", "control_addr" ]
-    if not in_vm_name in ok_vm_list:
-        exit("The argument to setup_vm must be a VM name from '{}'".format(" ".join(ok_vm_list)))
+    if not in_vm_name in OK_VM_LIST:
+        die("The argument to setup_vm must be a VM name from '{}'.".format(" ".join(OK_VM_LIST)))
     # Be sure this is running on an VM, not the control host
     this_hostname = subprocess.getoutput("hostname")
     if this_hostname != CLONE_BASENAME:
-        if this_hostname in ok_vm_list:
-            exit("This host's hostname is {}, which indicates it has already been set up.".format(this_hostname))
+        if this_hostname in OK_VM_LIST:
+            die("This host's hostname is {}, which indicates it has already been set up.".format(this_hostname))
         else:
-            exit("Weird: this hosts hostname is {}, which is not expected.".format(this_hostname))
+            die("Weird: this hosts hostname is {}, which is not expected.".format(this_hostname))
     print("Setting up {}".format(in_vm_name))
-    # Change the hostname to this_vm
     # Add the /etc/hosts/interfaces file
-    # Do VM-specific setup
+    this_interfaces_file = "{}/config-files/interfaces-{}".format(PROG_DIR, in_vm_name)
+    if not os.path.exists(this_interfaces_file):
+        die("The file {} does not exist, so cannot update this system.".format(this_interfaces_file))
+    try:
+        shutil.copy(this_interfaces_file, "/etc/network/interfaces")
+    except:
+        die("Could not copy {} to {}.".format(this_interfaces_file, "/etc/network/interfaces")
+    # For gateway-vm, add the firewall rules
+    ################################## More goes here
+    # Change the hostname to this_vm
+    try:
+        f = open("/etc/hostname", mode="wt")
+        f.write("{}\n".format(in_vm_name))
+        f.close()
+    except:
+        die("Failed to write out /etc/hostname.")
+    die("Finished. Reboot to bring up the new settings.")
+    

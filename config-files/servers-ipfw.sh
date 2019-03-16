bd05c2536e73352243be48ee35cb58d2e9b7caaf
Paul Hoffman <phoffman@proper.com>
2019-03-15 18:12:24 -0700

    New FreeBSD config files

---
+++ config-files/servers-ipfw.sh
@@ -0,0 +1,26 @@
+ipfw add 50101 pipe 101 ip from 172.21.101.1 to any
+ipfw add 50102 pipe 102 ip from 172.21.102.1 to any
+ipfw add 50103 pipe 103 ip from 172.21.103.1 to any
+ipfw add 50104 pipe 104 ip from 172.21.104.1 to any
+ipfw add 50105 pipe 105 ip from 172.21.105.1 to any
+ipfw add 50106 pipe 106 ip from 172.21.106.1 to any
+ipfw add 50107 pipe 107 ip from 172.21.107.1 to any
+ipfw add 50108 pipe 108 ip from 172.21.108.1 to any
+ipfw add 50109 pipe 109 ip from 172.21.109.1 to any
+ipfw add 50110 pipe 110 ip from 172.21.110.1 to any
+ipfw add 50111 pipe 111 ip from 172.21.111.1 to any
+ipfw add 50112 pipe 112 ip from 172.21.112.1 to any
+ipfw add 50113 pipe 113 ip from 172.21.113.1 to any
+ipfw pipe 101 config delay 1300
+ipfw pipe 102 config delay 1200
+ipfw pipe 103 config delay 1100
+ipfw pipe 104 config delay 1000
+ipfw pipe 105 config delay 900
+ipfw pipe 106 config delay 800
+ipfw pipe 107 config delay 700
+ipfw pipe 108 config delay 600
+ipfw pipe 109 config delay 500
+ipfw pipe 110 config delay 400
+ipfw pipe 111 config delay 300
+ipfw pipe 112 config delay 200
+ipfw pipe 113 config delay 100

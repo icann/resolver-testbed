b1f109d8df34d7477cf46fb34a9ea24765b23460
Yorgos Thessalonikefs <george@nlnetlabs.nl>
2022-12-11 01:58:19 +0100

    Parsing Ansible inventory for Vagrant

--- auto/helper_scripts/setup-servers-vm.sh
+++ auto/helper_scripts/setup-servers-vm.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-### Copy the normal files that will be the config after reboot
-echo "Copy the rc.conf"
-# This sets the hostname and makes the needed sshd config
-cp /vagrant/files/servers-rc.conf /etc/rc.conf  || exit
-echo "Set the resolver"
-cp /vagrant/files/resolv-with-8844 /etc/resolv.conf || exit
+#### Copy the normal files that will be the config after reboot
+#echo "Copy the rc.conf"
+## This sets the hostname and makes the needed sshd config
+#cp /vagrant/files/servers-rc.conf /etc/rc.conf  || exit
+#echo "Set the resolver"
+#cp /vagrant/files/resolv-with-8844 /etc/resolv.conf || exit
 
 # XXX This can happen later with Ansible
 #echo "Installing from pkg"
@@ -23,6 +23,7 @@ cp /vagrant/files/resolv-with-8844 /etc/resolv.conf || exit
 echo "Making boot faster; always autoboot"
 echo 'autoboot_delay="-1"' >/boot/loader.conf
 
-### Finish up echo "Shutting down"
+### Finish up
+echo "Shutting down"
 sleep 2
 shutdown -p now
